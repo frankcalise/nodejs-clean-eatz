ef1d4b7bc2a29bef18fe14684cc893bff7ea3981
Frank Calise <fcalise@gmail.com>
2018-09-07 10:18:33 -0400

    Added material-ui packages and nav bar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import DefaultLayout from "./components/DefaultLayout";
+import NavBar from "./components/NavBar";
 import Customers from "./pages/Customers";
 import Reports from "./pages/Reports";
 import registerServiceWorker from "./registerServiceWorker";
@@ -9,8 +11,9 @@ import registerServiceWorker from "./registerServiceWorker";
 ReactDOM.render(
   <Router>
     <div>
-      <Route exact path="/" component={Customers} />
-      <Route path="/reports/:id?" component={Reports} />
+      <NavBar />
+      <DefaultLayout exact path="/" component={Customers} />
+      <DefaultLayout path="/reports/:id?" component={Reports} />
     </div>
   </Router>,
   document.getElementById("root")
