5747a3334fe146518e93a1da7c0cc3570e3ea109
Frank Calise <fcalise@gmail.com>
2018-09-07 09:12:11 -0400

    Fixed report route rendering issue

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -2,14 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import NonOrderingCustomers from "./reports/NonOrderingCustomers";
 
-const REPORTS = {
-  1: NonOrderingCustomers
-};
+const reportComponents = { 1: NonOrderingCustomers };
 
 export default class Reports extends React.Component {
   render() {
     const id = this.props.match.params.id;
-    const ReportComponent = id ? REPORTS[id] : null;
+    const ReportComponent = id ? reportComponents[id] : null;
+
     return (
       <div className="reports">
         <h1>Reports</h1>
@@ -18,7 +17,7 @@ export default class Reports extends React.Component {
             <Link to="/reports/1">Non-ordering Customers</Link>
           </li>
         </ul>
-        <ReportComponent />
+        {ReportComponent ? <ReportComponent /> : null}
       </div>
     );
   }
