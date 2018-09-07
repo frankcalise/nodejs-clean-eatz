bd445cb246dbf99981ad5c32de131971f6efedca
Frank Calise <fcalise@gmail.com>
2018-09-07 18:34:23 -0400

    Added dashboard view to work on, order count summary chart

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./index.css";
 import DefaultLayout from "./components/DefaultLayout";
 import NavBar from "./components/NavBar";
+import Dashboard from "./pages/Dashboard";
 import Customers from "./pages/Customers";
 import Reports from "./pages/Reports";
 import registerServiceWorker from "./registerServiceWorker";
@@ -11,8 +12,9 @@ import registerServiceWorker from "./registerServiceWorker";
 ReactDOM.render(
   <Router>
     <div>
-      <NavBar />
-      <DefaultLayout exact path="/" component={Customers} />
+      {/* <NavBar /> */}
+      <DefaultLayout exact path="/" component={Dashboard} />
+      <DefaultLayout path="/customers" component={Customers} />
       <DefaultLayout path="/reports/:id?" component={Reports} />
     </div>
   </Router>,
---
+++ src/pages/dashboard/OrderCountSummaryChart.js
@@ -0,0 +1,80 @@
+import React from "react";
+import ResponsiveContainer from "recharts/lib/component/ResponsiveContainer";
+import LineChart from "recharts/lib/chart/LineChart";
+import Line from "recharts/lib/cartesian/Line";
+import XAxis from "recharts/lib/cartesian/XAxis";
+import YAxis from "recharts/lib/cartesian/YAxis";
+import CartesianGrid from "recharts/lib/cartesian/CartesianGrid";
+import Tooltip from "recharts/lib/component/Tooltip";
+import Legend from "recharts/lib/component/Legend";
+import fire from "../../fire";
+
+class OrderCountSummaryChart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentWillMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    fire
+      .database()
+      .ref("orderSummaries")
+      .orderByKey()
+      .once("value")
+      .then(snapshot => {
+        const data = [];
+
+        snapshot.forEach(childSnapshot => {
+          const summary = childSnapshot.val();
+          const { menuDate, numMeals, orderCount } = summary;
+
+          data.push({
+            menuDate,
+            numMeals,
+            orderCount
+          });
+        });
+
+        this.setState({ data });
+      });
+  };
+
+  render() {
+    const orderLabel = "# Orders";
+    const mealsLabel = "# Meals";
+    return (
+      // 99% per https://github.com/recharts/recharts/issues/172
+      <ResponsiveContainer width="99%" height={320}>
+        <LineChart data={this.state.data}>
+          <XAxis dataKey="menuDate" />
+          <YAxis />
+          <CartesianGrid vertical={false} strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            name="# Meals"
+            dataKey="numMeals"
+            stroke="#82ca9d"
+          />
+          <Line
+            type="monotone"
+            name="# Orders"
+            dataKey="orderCount"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+  }
+}
+
+export default OrderCountSummaryChart;
