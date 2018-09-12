76783d595c64e69d426da3ef5600b59b595c7ca8
Frank Calise <fcalise@gmail.com>
2018-09-12 19:51:11 -0400

    WIP making non ordering customers table sortable

--- src/pages/reports/NonOrderingCustomers.js
+++ src/pages/reports/NonOrderingCustomers.js
@@ -9,6 +9,7 @@ import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import moment from "moment";
+import EnhancedTableHead from "../../components/EnhancedTableHead";
 import fire from "../../utils/fire";
 
 const propTypes = {
@@ -26,13 +27,70 @@ const styles = theme => ({
   }
 });
 
+const cols = [
+  {
+    id: "name",
+    numeric: false,
+    disablePadding: true,
+    label: "Name",
+    isSortable: true
+  },
+  {
+    id: "lastOrderDate",
+    numeric: false,
+    disablePadding: true,
+    label: "Last Order Date",
+    isSortable: true
+  },
+  {
+    id: "email",
+    numeric: false,
+    disablePadding: true,
+    label: "Email"
+  },
+  {
+    id: "phone",
+    numeric: false,
+    disablePadding: true,
+    label: "Phone"
+  }
+];
+
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy);
+}
+
 class NonOrderingCustomers extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       daysSinceLastOrder: 30,
-      rows: []
+      rows: [],
+      order: "asc",
+      orderBy: "lastOrderDate"
     };
   }
 
@@ -75,9 +133,20 @@ class NonOrderingCustomers extends React.Component {
       });
   };
 
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = "desc";
+
+    if (this.state.orderBy === property && this.state.order === "desc") {
+      order = "asc";
+    }
+
+    this.setState({ order, orderBy });
+  };
+
   render() {
     const { classes } = this.props;
-    const { rows, daysSinceLastOrder } = this.state;
+    const { rows, daysSinceLastOrder, order, orderBy } = this.state;
 
     return (
       <Paper className={classes.root}>
@@ -85,19 +154,25 @@ class NonOrderingCustomers extends React.Component {
           Non Ordering Customers
         </Typography>
         <Typography variant="subheading" gutterBottom>
-          Last {daysSinceLastOrder} days
+          Last {daysSinceLastOrder} days, {rows.length} total customers
         </Typography>
         <Table className={classes.table}>
-          <TableHead>
+          <EnhancedTableHead
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={this.handleRequestSort}
+            cols={cols}
+          />
+          {/* <TableHead>
             <TableRow>
               <TableCell>Name</TableCell>
               <TableCell>Last Order Date</TableCell>
               <TableCell>Email</TableCell>
               <TableCell>Phone</TableCell>
             </TableRow>
-          </TableHead>
+          </TableHead> */}
           <TableBody>
-            {rows.map(row => {
+            {stableSort(rows, getSorting(order, orderBy)).map(row => {
               return (
                 <TableRow key={row.customerKey}>
                   <TableCell component="th" scope="row">
