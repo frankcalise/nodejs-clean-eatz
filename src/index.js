1b02705327d8a93a903a9d06d9808dea72499095
Frank Calise <fcalise@gmail.com>
2018-09-30 17:59:49 -0400

    Added SignIn component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./index.css";
 import DefaultLayout from "./components/DefaultLayout";
 import Dashboard from "./pages/dashboard";
 import Customers from "./pages/customers";
 import Reports from "./pages/reports";
 import Orders from "./pages/orders";
+import SignIn from "./pages/auth";
 import SuperCustom from "./pages/super_custom";
 import registerServiceWorker from "./registerServiceWorker";
 
@@ -18,6 +19,7 @@ ReactDOM.render(
       <DefaultLayout path="/reports/:id?" component={Reports} />
       <DefaultLayout path="/super-custom" component={SuperCustom} />
       <DefaultLayout path="/orders" component={Orders} />
+      <Route path="/sign-in" component={SignIn} />
     </Switch>
   </Router>,
   document.getElementById("root")
