dff095fa2293683e7f4a8ec3ce6d15523a1e89b2
Frank Calise <fcalise@gmail.com>
2018-10-18 19:45:59 -0400

    WIP merge customer script

--- firebase_scripts/merge_customer_records.js
+++ firebase_scripts/merge_customer_records.js
@@ -11,11 +11,13 @@ admin.initializeApp({
 });
 
 const customersRef = admin.database().ref("customers");
+const ordersRef = admin.database().ref("orders");
 
 const checkCustomerExistsByKey = key => {
+  const encodedKey = fbUtils.encodeAsFirebaseKey(key);
   return admin
     .database()
-    .ref("customers/" + key)
+    .ref("customers/" + encodedKey)
     .once("value")
     .then(snapshot => {
       return snapshot.exists();
@@ -23,9 +25,10 @@ const checkCustomerExistsByKey = key => {
 };
 
 const getCustomerByKey = key => {
-  console.log("find customer with key:", key);
+  const encodedKey = fbUtils.encodeAsFirebaseKey(key);
+  console.log("find customer with key:", encodedKey);
   return customersRef
-    .child(key)
+    .child(encodedKey)
     .once("value")
     .then(snapshot => {
       const customer = snapshot.val();
@@ -34,8 +37,9 @@ const getCustomerByKey = key => {
 };
 
 const args = process.argv;
-const primaryKey = fbUtils.encodeAsFirebaseKey(args[2]);
+const primaryKey = args[2];
 let secondaryKey = args[3];
+const mergedKey = args[4];
 
 async function main() {
   // if secondaryKey doesn't exist, just move primaryKey record to this
@@ -43,7 +47,7 @@ async function main() {
   const primaryCustomer = await getCustomerByKey(primaryKey);
   let secondaryCustomer = null;
   if (secondaryKey) {
-    secondaryKey = fbUtils.encodeAsFirebaseKey(secondaryKey);
+    // secondaryKey = fbUtils.encodeAsFirebaseKey(secondaryKey);
     const exists = await checkCustomerExistsByKey(secondaryKey);
     if (exists === true) {
       secondaryCustomer = await getCustomerByKey(secondaryKey);
@@ -53,23 +57,131 @@ async function main() {
   if (secondaryCustomer) {
     console.log(secondaryCustomer.name);
     // do comparisons
+    // first and last order
+
+    const combinedOrders = {
+      ...primaryCustomer.orders,
+      ...secondaryCustomer.orders
+    };
+
+    // merge first order datetime
+    let firstOrderData = {
+      firstOrderDate: null,
+      firstOrderId: null,
+      firstTransactionId: null
+    };
+
+    if (primaryCustomer.firstOrderDate && secondaryCustomer.firstOrderDate) {
+      // check which is earlier
+      if (primaryCustomer.firstOrderDate < secondaryCustomer.firstOrderDate) {
+        firstOrderData.firstOrderDate = primaryCustomer.firstOrderDate;
+        firstOrderData.firstOrderId = primaryCustomer.firstOrderId;
+        firstOrderData.firstTransactionId = primaryCustomer.firstTransactionId;
+      } else {
+        firstOrderData.firstOrderDate = secondaryCustomer.firstOrderDate;
+        firstOrderData.firstOrderId = secondaryCustomer.firstOrderId;
+        firstOrderData.firstTransactionId =
+          secondaryCustomer.firstTransactionId;
+      }
+    } else if (
+      primaryCustomer.firstOrderDate &&
+      !secondaryCustomer.firstOrderDate
+    ) {
+      firstOrderData.firstOrderDate = primaryCustomer.firstOrderDate;
+      firstOrderData.firstOrderId = primaryCustomer.firstOrderId;
+      firstOrderData.firstTransactionId = primaryCustomer.firstTransactionId;
+    } else if (
+      !primaryCustomer.firstOrderDate &&
+      secondaryCustomer.firstOrderDate
+    ) {
+      firstOrderData.firstOrderDate = secondaryCustomer.firstOrderDate;
+      firstOrderData.firstOrderId = secondaryCustomer.firstOrderId;
+      firstOrderData.firstTransactionId = secondaryCustomer.firstTransactionId;
+    } else {
+      // don't have the info
+      console.log("no first date info");
+      console.log(primaryCustomer.firstOrderDate);
+      console.log(secondaryCustomer.firstOrderDate);
+    }
+
+    // merge last order datetime
+    let lastOrderData = {
+      lastOrderDate: null,
+      lastOrderId: null,
+      lastTransactionId: null
+    };
+    const primaryLastOrderDate = moment(
+      primaryCustomer.lastOrderDate
+    ).valueOf();
+    const secondaryLastOrderDate = moment(
+      secondaryCustomer.lastOrderDate
+    ).valueOf();
+    if (primaryLastOrderDate > secondaryLastOrderDate) {
+      lastOrderData.lastOrderDate = primaryCustomer.lastOrderDate;
+      lastOrderData.lastOrderId = primaryCustomer.lastOrderId;
+      lastOrderData.lastTransactionId = primaryCustomer.lastTransactionId;
+    } else {
+      lastOrderData.lastOrderDate = secondaryCustomer.lastOrderDate;
+      lastOrderData.lastOrderId = secondaryCustomer.lastOrderId;
+      lastOrderData.lastTransactionId = secondaryCustomer.lastTransactionId;
+    }
+
+    const combinedCustomer = {
+      ...primaryCustomer,
+      email: primaryCustomer.email.toLowerCase(),
+      orders: combinedOrders,
+      ...firstOrderData,
+      ...lastOrderData
+    };
+
+    console.log(combinedCustomer);
+    const lowerKey = fbUtils.encodeAsFirebaseKey(mergedKey.toLowerCase());
+
+    const orderUpdates = Object.keys(combinedCustomer.orders).map(orderKey => {
+      const transactionKey = combinedCustomer.orders[orderKey];
+      return `${orderKey}/${transactionKey}`;
+    });
+    // foreach primaryCustomer.orders
+    console.log(orderUpdates);
+    orderUpdates.forEach(orderPath => {
+      //ordersRef.child(orderPath).update({ customerKey: lowerKey })
+    });
+
+    console.log("merged key", lowerKey);
+    // customersRef.child(lowerKey).set(primaryCustomer);
+    const encodedPrimaryKey = fbUtils.encodeAsFirebaseKey(primaryKey);
+    const encodedSecondaryKey = fbUtils.encodeAsFirebaseKey(secondaryKey);
+    // customersRef.child(encodedPrimaryKey).remove();
+    // customersRef.child(encodedSecondaryKey).remove();
   } else {
     console.log("just move primary");
-    const lowerKey = fbUtils.encodeAsFirebaseKey(primaryKey.toLowerCase());
+    const lowerKey = fbUtils.encodeAsFirebaseKey(
+      (secondaryKey || primaryKey).toLowerCase()
+    );
     console.log("to key", lowerKey);
     console.log(primaryCustomer);
     console.log("switch orders from primary key");
     const orderKeys = Object.keys(primaryCustomer.orders);
     const orderUpdates = orderKeys.map(orderKey => {
       const transactionKey = primaryCustomer.orders[orderKey];
-      return `orders/${orderKey}/${transactionKey}/customerKey`;
+      return `${orderKey}/${transactionKey}`;
     });
     // foreach primaryCustomer.orders
     console.log(orderUpdates);
+    orderUpdates.forEach(orderPath => {
+      //ordersRef.child(orderPath).update({ customerKey: lowerKey })
+    });
     // /orders/orderKey/transactionKey/customerKey
     // update to lowerKey
     console.log("remove customer at primaryKey");
     // customersRef.child(lowerKey).set(primaryCustomer);
+    const newProps = {
+      ...primaryCustomer,
+      email: secondaryKey.toLowerCase()
+    };
+    console.log(newProps);
+
+    // customersRef.child(primaryKey).remove();
   }
 }
 
